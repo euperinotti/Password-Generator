025970699e3b5e1e75ee6f6118cd78ed254e1b4b
Guilherme Perinotti <guilherme9115@hotmail.com>
2022-07-15 07:52:24 -0300

    code commented

--- script.js
+++ script.js
@@ -1,39 +1,44 @@
-const maiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-const minusculas = 'abcdefghijklmnopqrstuvwxyz'; 
+const upperChar = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const lowerChar = 'abcdefghijklmnopqrstuvwxyz'; 
 const nums = '0123456789'; 
-const especiais = '!\"#$%()*+,-./:;<=>?@[]{}_~';
-const button = document.getElementById('button')
+const specialChar = '!"\\#$%()"*+,-./:;<=>?@[]{}_~|';
 
-let filter = [nums];
-let length = document.getElementById('password_length');
-let checks = document.querySelectorAll('.filter_check');
-
-
+let button = document.getElementById('button');
+let filter = [nums]; //array with chars
+let length = document.getElementById('password_length'); //get length of the password
+let checks = document.querySelectorAll('.filter_check'); //get the filters of the password
 
 button.addEventListener('click', () => {
+
+    // first, check if the filters are checked
+    // then add the filters to the array
+    // execute the function to generate de new password
+
     if(checks[0].name == "upper_case" && checks[0].checked == true){
-        filter.push(maiusculas);
+        filter.push(upperChar);
     }
 
     if(checks[1].name == "lower_case" && checks[1].checked == true){
-        filter.push(minusculas);
+        filter.push(lowerChar);
     }
 
     if(checks[2].name == "special_characters" && checks[2].checked == true){
-        filter.push(especiais);
+        filter.push(specialChar);
     }
     document.querySelector('p').innerHTML = generatePassword(parseInt(length.value));
+
+    // reset the array
 
     filter = [nums]
 })
 
-function generatePassword(_length){
+function generatePassword(_password_length){
     let password = '';
     let randomInt;
-    while(_length > 0){
+    while(_password_length > 0){
         randomInt = Math.floor(Math.random() * filter.length)
         password = password + filter[randomInt].charAt(Math.floor(Math.random() * filter[randomInt].length));
-        _length--
+        _password_length--
     }
     return password;
 }
